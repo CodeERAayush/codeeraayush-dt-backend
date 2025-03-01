2df5cfcde83662470977f67125db84aef2729857
codeeraayush <aayushpandey1100@gmail.com>
2025-03-01 13:19:53 +0530

    changes

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://aayushpandey1100:CfklSYdZ2O9Y0toV@developerstinder.zeatc.mongodb.net/"
+    process.env.MONGO_URL
   );
 };
 
